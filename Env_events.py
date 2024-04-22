a6aa7e0c797907a51b5d5915f2107f591d7a0fbe
Thabo Mohapi <thabomohapi012@gmail.com>
2024-04-22 23:21:20 +0200

    added functionality for human mode and AI mode

--- Env_events.py
+++ Env_events.py
@@ -2,8 +2,9 @@ class EventManager:
     def __init__(self, engine: object) -> None:
         self.engine = engine
 
-    def handle_events(self) -> None:
+    def handle_events(self, action = None) -> None:
         self.events = self.engine.e.event.get()
+        # if action is not None:
         self.keys = self.engine.e.key.get_pressed()
         
         for event in self.events:
@@ -12,10 +13,14 @@ class EventManager:
                 break
 
             if event.type == self.engine.SCREEN_UPDATE:
-                self.engine.update()
+                self.engine.update_env_state()
 
-    def handle_keys(self) -> None:
-        for key, direction in self.engine.key_map.items():
-            if self.keys[key] and direction + self.engine.snake.head != self.engine.snake.body[1]: # check if direction is valid and not to the snake's neck
-                self.engine.snake.direction = direction
-                break
+    def handle_keys(self, action = None) -> None:
+        if action is None:
+            for key, direction in self.engine.key_map.items():
+                if self.keys[key] and direction + self.engine.snake.head != self.engine.snake.body[1]: # check if direction is valid and not to the snake's neck
+                    self.engine.snake.direction = direction
+                    break
+        else: 
+            self.engine.snake.direction = action
+            # self.engine.update_env_state()
