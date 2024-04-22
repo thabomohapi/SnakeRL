16f4dec7ef06c38877041aba3ab7484f16ea9471
Thabo Mohapi <thabomohapi012@gmail.com>
2024-04-22 23:22:52 +0200

    added a classical AI agent

--- Agent.py
+++ Agent.py
@@ -1,23 +1,55 @@
+import Env, sys, random
+from Search import a_star_search
+
 class Agent:
-    def __init__(self, model, environment) -> None:
-        self.model = model
-        self.env = environment
+    def __init__(self, engine: object) -> None:
+        self.engine = engine
+        self.num_games = 0
+        self.grid_size = self.engine.cell_number
+        self.state = self.get_state()
 
     def get_state(self) -> None:
-        # get the current state of the environment...
-        # i.e snake position(s), food positions(s) and obstacles
-        pass
+        raise NotImplementedError("get_state method must be implemented!")
 
-    def choose_action(self) -> None:
-        # use trained model to get best action given a state
-        pass
+    def choose_action(self) -> 'Env.Vec2':
+        raise NotImplementedError("choose_action method must be implemented!")
 
     def play(self) -> None:
-        # play game based on training or...
-        # train model based on env state and reward
-        pass
+        raise NotImplementedError("play method must be implemented!")
 
-# model = ... # Load trained model
-# env = ... # set up environment
-# agent = # Agent(model, env)
-# agent.play()
+class SearchAgent(Agent):
+    def __init__(self, engine: object) -> None:
+        super().__init__(engine)
+
+    def get_state(self) -> None:
+        return self.engine.env_state
+
+    def choose_action(self) -> Env.Vec2:
+        head = self.state['snake'][0]
+        food = self.state['food'][0]
+        obstacles = [x for y in self.state['obstacles'] for x in y] # convert to 1D list
+        obstacles += self.state['snake']
+        obstacles.append(self.state['food'][1])
+        path = a_star_search(head, food, obstacles, self.grid_size)
+        if path is None:
+            return random.choice([Env.Vec2(0, -1), Env.Vec2(0, 1), Env.Vec2(-1, 0), Env.Vec2(1, 0)])
+        return head.negate() + path[1]
+    
+    def play(self) -> None:
+        self.engine.e.time.set_timer(self.engine.SCREEN_UPDATE, 105)
+        self.engine.running = True
+        while self.engine.running:
+            action = self.choose_action()
+            self.engine.event_manager.handle_events(action)
+                
+            if self.engine.death:
+                self.engine.reset_game()
+            
+            self.engine.event_manager.handle_keys(action)
+            self.engine.renderer.update_high_score()
+            self.engine.renderer.draw()
+            self.engine.e.display.flip() # use flip instead of update for a complete frame update
+            self.engine.clock.tick(self.engine.fps) # set framerate
+        
+        self.engine.e.quit()
+        sys.exit()
