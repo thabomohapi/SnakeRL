24b7bba0ec48a164ee274d2f1faea88c41a379ea
Thabo Mohapi <thabomohapi012@gmail.com>
2024-04-22 23:21:28 +0200

    added functionality for human mode and AI mode

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
 from Env import Engine
 from Env_contoller import GameController
+from Agent import SearchAgent
 
 if __name__ == "__main__":
     game_engine = Engine()
-    game_controller = GameController(game_engine)
-    game_controller.run_game()
+    
+    if len(sys.argv) == 1 or sys.argv[1] == 'ai':
+        agent = SearchAgent(game_engine)
+        agent.play()
+    elif sys.argv[1] == 'human':
+        game_controller = GameController(game_engine)
+        game_controller.run_game()
