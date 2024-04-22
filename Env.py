8641230a91e75593ceb9b6887f0dc89a6e06b595
Thabo Mohapi <thabomohapi012@gmail.com>
2024-04-22 23:19:22 +0200

    added an environment state capture

--- Env.py
+++ Env.py
@@ -2,9 +2,17 @@ from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 import pygame # type: ignore
-from Env_models import Snake, Food, Obstacle, Vec2
 from Env_view import Renderer
+from Env_models import Snake, Food, Obstacle, Vec2
 from Env_events import EventManager
+from Env_contoller import GameController
+
+DIRECTIONS = {
+    'UP': Vec2(0, -1),
+    'RIGHT': Vec2(1, 0),
+    'LEFT': Vec2(-1, 0),
+    'DOWN':Vec2(0, 1)
+}
 
 class Engine:
     _instance = None
@@ -36,6 +44,7 @@ class Engine:
         self.running = False
         self.key_map = self.create_key_map()
         self.start_timer = pygame.time.get_ticks()
+        self.env_state = {}
 
     def initialize_game_objects(self):
         self.initialize_obstacles()
@@ -47,12 +56,15 @@ class Engine:
     def initialize_obstacles(self) -> None:
         self.obstacles = []
         self.obstacles = 15 * Obstacle(self, 5)
+        self.env_state['obstacles'] = [ x.blocks for x in self.obstacles ]
 
     def initialize_agents(self) -> None:
         self.snake = Snake(self)
+        self.env_state['snake'] = self.snake.body
 
     def initialize_food(self) -> None:
         self.fruits = (Food(True, self), Food(False, self))
+        self.env_state['food'] = [ x.position for x in self.fruits ]
 
     def initialize_event_manager(self) -> None:
         self.event_manager = EventManager(self)
@@ -62,35 +74,36 @@ class Engine:
 
     def create_key_map(self):
         return {
-            self.e.K_w: Vec2(0, -1),
-            self.e.K_UP: Vec2(0, -1),
-            self.e.K_d: Vec2(1, 0),
-            self.e.K_RIGHT: Vec2(1, 0),
-            self.e.K_a: Vec2(-1, 0),
-            self.e.K_LEFT: Vec2(-1, 0),
-            self.e.K_s: Vec2(0, 1),
-            self.e.K_DOWN: Vec2(0, 1)
+            self.e.K_w: DIRECTIONS['UP'],
+            self.e.K_UP: DIRECTIONS['UP'],
+            self.e.K_d: DIRECTIONS['RIGHT'],
+            self.e.K_RIGHT: DIRECTIONS['RIGHT'],
+            self.e.K_a: DIRECTIONS['LEFT'],
+            self.e.K_LEFT: DIRECTIONS['LEFT'],
+            self.e.K_s: DIRECTIONS['DOWN'],
+            self.e.K_DOWN: DIRECTIONS['DOWN']
         }
 
     def load_image(self, path: str) -> None:
         image = self.e.image.load(path).convert_alpha()
         return self.e.transform.scale(image, (self.width, self.height))
     
-    def update(self) -> None:
+    def update_env_state(self) -> None:
         if not self.snake.direction.zero:
             self.snake.move()
+            self.env_state['snake'] = self.snake.body
         self.check_collision()
-        self.fruits[1].update()
-        for obs in self.obstacles:
-            obs.update_occupied_positions()
+        for i, v in enumerate(self.fruits):
+            self.fruits[i].update(self.env_state)
+            self.env_state['food'][i] = v.position
 
     def check_collision(self) -> None:
         head = self.snake.head
         if head == self.fruits[0].position:
-            self.fruits[0].update()
+            self.fruits[0].update(self.env_state, True)
             self.snake.grow = True
         elif head == self.fruits[1].position:
-            self.fruits[1].update(True)
+            self.fruits[1].update(self.env_state, True)
             self.snake.shrink = True
         elif self.snake.snakeLength <= 1 or len(self.snake.body) != len(set(self.snake.body)):
             self.death = True
@@ -111,7 +124,8 @@ class Engine:
         self.occupied_positions = set(positions)
 
     def reset_game(self) -> None:
-        self.snake.reset()
+        self.snake.reset(self.env_state)
         for fruit in self.fruits:
-            fruit.update(self.death)
+            fruit.update(self.env_state, self.death)
         self.death = False
+        print(self.env_state)
